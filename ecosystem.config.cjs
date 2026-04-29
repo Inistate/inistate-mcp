7dfb31055a545e031f36d2355e94f33d2e308519
dylon-inistate <dylon.wongchungyee@inistate.com>
2026-04-29 18:22:39 +0800

    Add log rotation

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,9 +9,21 @@ module.exports = {
       instances: 1,
       autorestart: true,
       max_memory_restart: "300M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
       env: {
         NODE_ENV: "production",
       },
     },
   ],
+  deploy: {},
+  module_conf: {
+    "pm2-logrotate": {
+      max_size: "10M",
+      retain: "7",
+      compress: true,
+      dateFormat: "YYYY-MM-DD_HH-mm-ss",
+      workerInterval: "3600",
+      rotateInterval: "0 0 * * *",
+    },
+  },
 };
